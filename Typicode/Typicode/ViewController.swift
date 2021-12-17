90eb98ed931afb069f2a6999137cb0a9c0954f2b
Reynaldo Hutahaean <reynaldo@sgo.co.id>
2021-12-17 08:12:13 +0700

    first init

---
+++ Typicode/Typicode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Typicode
+//
+//  Created by Reynaldo Cristinus Hutahaean on 17/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
